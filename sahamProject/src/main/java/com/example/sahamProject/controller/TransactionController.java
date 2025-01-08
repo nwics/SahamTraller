5d275c7b34ecb3294eedc1d56a411fd993f56a62
nwics <nabilwicaksono274@gmail.com>
2025-01-08 20:19:12 +0700

    edit ui for page

--- sahamProject/src/main/java/com/example/sahamProject/controller/TransactionController.java
+++ sahamProject/src/main/java/com/example/sahamProject/controller/TransactionController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/faq")
-public class FaqController {
+@RequestMapping("/transaction")
+public class TransactionController {
     @GetMapping("/index")
     public String index() {
-        return "faq/index";
+        return "transaction/index";
     }
 }
