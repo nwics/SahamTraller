8601ccf726714eb24008b2842d50248b5d1ad2b4
nwics <nabilwicaksono274@gmail.com>
2025-01-11 21:24:51 +0700

    be register

--- sahamProject/src/main/java/com/example/sahamProject/model/MCustomer.java
+++ sahamProject/src/main/java/com/example/sahamProject/model/MCustomer.java
@@ -9,12 +9,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
+@Table(name = "m_customer")
 public class MCustomer extends BaseEntity {
 
     @Id
